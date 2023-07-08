80512b54dd3c9d87e305e7222a9de6f33e20bae6
Kristi Cornett <kristi@cornettclan.com>
2023-07-08 14:04:04 -0500

    working on landing page

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -1,33 +1,55 @@
+import { useState } from 'react'
 import './Home.css'
 
 
 export const Home = () => {
     const [task, setTask] = useState('')
+    const [toDo, setToDo] = useState([])
 
+    const handleSubmit = (evt) => {
+        evt.preventDefault()
+    
+        setToDo(currentToDo => {
+            return [
+                ...currentToDo, 
+                { id: crypto.randomUUID(), title: task, completed: false},
+            ]
+        })
+        setTask('')
+    }
 
+    const toggleToDo = (id, completed) => {
+        setToDo(currentToDo => {
+            return currentToDo.map(todo => {
+                if(toDo.id === id){
+                    return {...todo, completed}
+                }
+                return todo
+            })
+        })
 
     return <>
     <h1 className='header'>To Do List</h1>
-        <form className='new-item-form'>
+        <form onSubmit={handleSubmit} className='new-item-form'>
             <div className='form-row'>
                 <label htmlFor='item'>New Item</label>
-                <input value={task} type='text' id='item' />
+                <input value={task} onChange={evt => setTask(evt.target.value)} type='text' id='item' />
             </div>
             <button className='btn'>Add</button>
         </form>
         <ul className='list' style={{listStyle: 'none'}}>
-            <li>
-                <label>
-                    <input type='checkbox' />
-                    Item 1
-                </label>
-            </li>
-            <li>
-                <label>
-                    <input type='checkbox' />
-                    Item 2
-                </label>
-            </li>
+            {
+                toDo.map(t => {
+                    return  <li key={t.id}>
+                    <label>
+                        <input type='checkbox' checked={t.completed}
+                        onChange={(evt => toggleToDo(toDo.id, evt.target.checked))}/>
+                        {t.title}
+                    </label>
+                </li>
+                })
+            }
+           
         </ul>
         
     </>
